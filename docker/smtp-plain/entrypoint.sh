dba97b99dcb350e381ab5096f1408361b250b3ba
KasuyaMofu <56345674+KasuyaMofu@users.noreply.github.com>
2026-01-12 18:02:31 +0900

    milter導入

--- docker/smtp-plain/entrypoint.sh
+++ docker/smtp-plain/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# SMTPプロトコルキャプチャをバックグラウンドで開始
+MYHOSTNAME=$(cat /etc/mailname)
+
+# SMTPコマンドログ用milterをバックグラウンドで開始
 rsyslogd
-(tshark -p -l -i eth0 -t a -T text -f "tcp port 25" -Y smtp | sed -u -E -e 's/→ [0-9.]+  //' -e 's/^[0-9:. ]+10\.255/10\.255/' -e 's/\/IMF//' -e 's/ SMTP [0-9]+ / SMTP /' | logger -t smtp-dump) &
+/usr/local/bin/smtp-command-output-milter -listen inet:10025@127.0.0.1 -hostname "$MYHOSTNAME" 2>&1 | logger -t milter &
 
 postfix start
-tail ---disable-inotify -f /var/log/syslog
+sleep infinity
--- docker/smtp-rspamd/entrypoint.sh
+++ docker/smtp-rspamd/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+MYHOSTNAME=$(cat /etc/mailname)
+
+# SMTPコマンドログ用milterをバックグラウンドで開始
+rsyslogd
+/usr/local/bin/smtp-command-output-milter -listen inet:10025@127.0.0.1 -hostname "$MYHOSTNAME" 2>&1 | logger -t milter &
+
 redis-server --daemonize yes
 sudo -u rspamd /usr/bin/rspamd -c /etc/rspamd/rspamd.conf
 postfix start
-tail ---disable-inotify -s 3 -f /var/log/postfix 
+tail ---disable-inotify -f /var/log/syslog
