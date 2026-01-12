f8c46c3c96504ebd6e833446042b26b80dcd15bc
KasuyaMofu <56345674+KasuyaMofu@users.noreply.github.com>
2026-01-12 21:49:39 +0900

    milter から proxy に載せ替え

--- docker/imap/entrypoint.sh
+++ docker/imap/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-MYHOSTNAME=$(cat /etc/mailname)
+# 自身のIPアドレスを取得
+SERVER_IP=$(hostname -i | awk '{print $1}')
 
-# SMTPコマンドログ用milterをバックグラウンドで開始
+# 受信プロキシをバックグラウンドで開始
 rsyslogd
-/usr/local/bin/smtp-command-output-milter -listen inet:10025@127.0.0.1 -hostname "$MYHOSTNAME" 2>&1 | logger -t milter &
+/usr/local/bin/smtp-receive-proxy \
+    -incoming :25 \
+    -postfix 127.0.0.1:10025 \
+    -ip "$SERVER_IP" \
+    2>&1 | logger -t receive-proxy &
 
 postfix start
+dovecot
 sleep infinity
